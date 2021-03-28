2cdcae68c23c53bd8c14e973d3d5ec0ef237a665
Suresh T R (Americas 1 - iDEAS-Cloud Transformation) <su40028014@wipro.com>
2021-03-28 20:03:55 +0530

    frontend

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,22 +3,22 @@ import {Navbar, Nav, Container} from 'react-bootstrap'
 import {LinkContainer} from 'react-router-bootstrap'
 const Header = () => {
     return (
-        <header>
-           <Navbar  bg="dark" variant='dark' collapseOnSelect>
+        <>
+           <Navbar bg="dark" variant='dark' collapseOnSelect>
 <Container> 
   <LinkContainer to = '/'> 
-  <Navbar.Brand>ProShop</Navbar.Brand>
+  <Navbar.Brand className='py-0'>ProShop</Navbar.Brand>
   </LinkContainer>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="ml-auto">
     <LinkContainer to = '/cart'> 
-      <Nav.Link>
+      <Nav.Link className='py-0'>
           <i className='pl-10 fas fa-shopping-cart'></i>
       Cart</Nav.Link>
       </LinkContainer>
       <LinkContainer to ='/login'>
-      <Nav.Link>
+      <Nav.Link className='py-0'>
           <i className='fas fa-user'></i>
       Sign In</Nav.Link>
       </LinkContainer>
@@ -26,7 +26,7 @@ const Header = () => {
   </Navbar.Collapse>
   </Container>
 </Navbar>
-        </header>
+        </>
     )
 }
 
