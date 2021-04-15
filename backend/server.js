8c917a7bc908bcb5da147272b3f5622ce72dc6e9
Suresh T R (Americas 1 - iDEAS-Cloud Transformation) <su40028014@wipro.com>
2021-04-15 14:05:38 +0530

    Heroku setup

--- backend/server.js
+++ backend/server.js
@@ -22,18 +22,29 @@ var razorpay = new Razorpay({
     key_secret: process.env.KEY_SECRET
 })
 
-app.get('/', (req, res) =>{
-    res.send("API is running")
-})
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
 
+app.use('/uploads', express.static(path.join(path.resolve(), '/uploads')))
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(path.resolve(), '/frontend/build')))
+    //any routes apart from the mentioned routes
+    app.get('*',(req,res) => res.sendFile(path.resolve(path.resolve(),'frontend','build','index.html')))
+}
+else {  
+    app.get('/', (req, res) =>{
+        res.send("API is running")
+    })
+}
+
+
 //This makes uploads folder as static so we can access from the browser 
 //let __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(path.resolve(), '/uploads')))
+
 
 // app.post('/razorpay', async (req, res) =>{
     
